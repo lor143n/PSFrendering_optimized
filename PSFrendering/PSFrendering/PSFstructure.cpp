ee7f995d76c37426000964d4ac031456b093aa3a
Lorenzo Piarulli <lorenzo.piarulli3000@gmail.com>
2023-06-04 13:02:15 +0200

    PSFconvolution: depth selection, normalization and convolution

--- PSFrendering/PSFrendering/PSFstructure.cpp
+++ PSFrendering/PSFrendering/PSFstructure.cpp
@@ -54,32 +54,83 @@ void makePSFsDictionary(path& p, std::vector<DepthDatabase>& depths) {
 
 //PSF convolution functions
 
-void psfConvolution(cv::Mat& rgb_image, cv::Mat& depth_image) {
+void psfConvolution(cv::Mat& rgb_image, cv::Mat& depth_image, cv::Mat& out_image, std::vector<DepthDatabase> data, int krnl_size) {
 
-    /*
-    src_image.forEach<Pixel>([](Pixel& p, const int* position) -> void {
-        
-        p.y = 255;
-        
-    });
-    */
-    /*
-    for (int r = 0; r < src_image.rows; ++r) {
-        Pixel* ptr = src_image.ptr<Pixel>(r, 0);
-        const Pixel* ptr_end = ptr + src_image.cols;
-        for (; ptr != ptr_end; ++ptr) {
-            ptr->x = 255;
-            ptr->y = 255;
-            ptr->z = 255;
-        }
-    }
-    */
 
-    for (int i = 0; i < rgb_image.rows; i++) {
+    int krnl_range = (krnl_size - 1) / 2;
+    
 
-        for (int j = 0; j < rgb_image.cols; j++) {
+    for (int i = krnl_range; i < rgb_image.rows - krnl_range; i++) {
+
+        for (int j = krnl_range; j < rgb_image.cols - krnl_range ; j++) {
         
             //std::cout << src_image.at<cv::Vec3b>(i, j) << std::endl;
+
+            //KERNEL BUILDING
+
+            cv::Mat krnl;
+            float krnl_sum = .0;
+
+            for (int h = 0; h < krnl_size; h++) {
+                for (int k = 0; k < krnl_size; k++) {
+
+                    //depth selection //convert getDepth() in float
+
+
+                    int high_dep_idx = 0;
+                    int low_dep_idx = 0;
+
+                    float dep = depth_image.at<cv::float16_t>(i - krnl_range + h, j - krnl_range + k);
+
+                    for (int dep_idx = 0; dep_idx < data.size(); dep_idx++) {
+
+
+                        if (dep_idx == data.size() - 1) {
+                            high_dep_idx = data.size() - 1;
+                            low_dep_idx = high_dep_idx - 1;
+                            break;
+                        }
+
+                        if (dep_idx == 0 && dep < data[dep_idx].getDepth()) {
+                            high_dep_idx = dep_idx + 1;
+                            low_dep_idx = dep_idx;
+                            break;
+                        }
+
+                        if (dep >= data[dep_idx].getDepth() && dep <= data[dep_idx+1].getDepth()) {
+                            high_dep_idx = dep_idx + 1;
+                            low_dep_idx = dep_idx;
+                            break;
+                        }
+
+                    }
+
+                    float high_depth = data[high_dep_idx].getDepth();
+                    float low_depth = data[low_dep_idx].getDepth();
+
+
+                    //Position Selection
+
+                    //Interpolation (by Position and by Depth)
+
+
+                }
+            }
+
+
+            //Kernel normalization
+            krnl = krnl / krnl_sum;
+
+            //Pixel convolution
+
+            for (int x = 0; x < krnl_size; x++) {
+                for (int y = 0; y < krnl_size; y++) {
+
+                    out_image.at<cv::Vec3f>(i - krnl_range, j - krnl_range) = krnl.at<cv::float16_t>(x, y) * rgb_image.at<cv::Vec3f>(i - krnl_range + x, j - krnl_range + y);
+                
+                }
+            }
+
         }
 
     }
